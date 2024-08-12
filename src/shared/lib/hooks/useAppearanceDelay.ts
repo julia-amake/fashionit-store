825a638f6e7a40b38260bd57e5e0025bb91eba8f
Yulia Starchenko <imay@amake.ru>
2024-08-12 18:06:16 +0300

    feat: implemented appearance delay

---
+++ src/shared/lib/hooks/useAppearanceDelay.ts
@@ -0,0 +1,30 @@
+import { startTransition, useEffect, useRef, useState } from 'react';
+
+interface AppearanceDelayOptions {
+  defaultValue?: boolean;
+  appearanceDelay?: number;
+  minDisplay?: number;
+}
+
+export const useAppearanceDelay = (
+  show?: boolean,
+  { minDisplay = 500, defaultValue = false, appearanceDelay = 500 }: AppearanceDelayOptions = {}
+) => {
+  const [shown, setShown] = useState(defaultValue);
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  useEffect(() => {
+    timerRef.current = setTimeout(
+      () => {
+        startTransition(() => setShown(!!show));
+      },
+      show ? appearanceDelay : minDisplay
+    );
+
+    return () => {
+      if (timerRef.current) clearTimeout(timerRef.current);
+    };
+  }, [appearanceDelay, minDisplay, show]);
+
+  return shown;
+};
